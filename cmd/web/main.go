00fefaedc19654dbd1cf2f52322fb2ee82ac0f83
Truong Minh Duc <ypvt123@gmail.com>
2023-08-07 09:15:47 +0700

    db connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/jackc/pgx/v5"
 )
 
 type application struct {
@@ -16,10 +19,19 @@ func main() {
 
 	// flag will be stored in the addr variable at runtime.
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "postgres://web:abc123@localhost:5432/snippetbox", "PostgreSQL data source name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	//Close the pool before the main() function exits.
+	defer db.Close(context.Background())
 
 	// Initialize a new instance of application struct, containing the dependencies.
 	app := &application{
@@ -34,6 +46,17 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*pgx.Conn, error) {
+	db, err := pgx.Connect(context.Background(), dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(context.Background()); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
